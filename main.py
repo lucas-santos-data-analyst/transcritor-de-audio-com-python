7776cb2585c80d350fc5b71b4634e07f8efa6cfa
Lucas Adriano <145597348+lucas-santos-data-analyst@users.noreply.github.com>
2026-09-16 01:19:42 -0300

    Melhoria de sintaxe e lógica de código

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import whisper
 from whisper.utils import get_writer
 
 agent = whisper.load_model("base")
+arquivo="ccs_150926_pt2"
+caminho = f"./src/audios/{arquivo}.m4a"
 
 response = agent.transcribe(
-  "./src/audios/aula-ccs-agentes-inteligentes.m4a", 
-  fp16=False)
+  caminho,
+  fp16=False,
+  initial_prompt="""
+  Escreva de forma legível, bem pontuado e usando quebras de linhas.
+  Faça duas quebras de linhas a cada ponto final.
+  Toda transcrição desse ser feita em português brasileiro""")
 
-with open("./src/outputs/transcrição.txt", "w", encoding="utf-8") as f:
+with open(f"./src/outputs/{arquivo}.txt", "w", encoding="utf-8") as f:
   f.write(response["text"])
 
 print("Transcrição concluída")
